fdad2868ab3ee0d950d4ce21d66554701ddebeaa
Diego-R-Ramos-M <diego.r.ramos.m@gmail.com>
2024-10-21 05:26:20 -0500

    Barra vertical añadida
    Cree un componente BarraVertical.jsx y una hoja de estilo BarraVertical.css. Asimismo, en la página Tienda.jsx, añadí un contenedor con 2 columnas, de modo que en la primera columna vaya la barra vertical creada y en la segunda, el resto de la página que queda pendiente. Por último, en el componente BarraDeNavegación.jsx, agregué una nueva clase para usarla en su css y ya no usar la etiqueta <a> en el css para que no afecte al resto de etiquetas <a>

--- src/componentes/BarraDeNavegacion.jsx
+++ src/componentes/BarraDeNavegacion.jsx
@@ -10,18 +10,18 @@ const BarraDeNavegacion = () => {
                 </a>
 
                 {/* Boton que aparecerá al colapsar el div posterior*/}
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
                 {/* Div colapsable*/}
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <div className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <a className="nav-link" href="/">Tienda</a>
-                        <a className="nav-link" href="/comunidad">Comunidad</a>
-                        <a className="nav-link" href="/acerca_de">Acerca de</a>
-                        <a className="nav-link" href="/soporte">Soporte</a>
-                        <a className="nav-link" href="/iniciar_sesion">Iniciar sesion</a>
+                        <a className="nav-link nav-enlace-horizontal" href="/">Tienda</a>
+                        <a className="nav-link nav-enlace-horizontal" href="/comunidad">Comunidad</a>
+                        <a className="nav-link nav-enlace-horizontal" href="/acerca_de">Acerca de</a>
+                        <a className="nav-link nav-enlace-horizontal" href="/soporte">Soporte</a>
+                        <a className="nav-link nav-enlace-horizontal" href="/iniciar_sesion">Iniciar sesion</a>
                     </div>
                 </div>
             </div>
