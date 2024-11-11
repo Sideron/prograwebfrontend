351a05f9ab2b7dc887b9cad454852b47e519b19b
Sebas-759 <sebastian_09_2003.0@hotmail.com>
2024-11-10 20:35:12 -0500

    xd

--- src/paginas/Biblioteca.jsx
+++ src/paginas/Biblioteca.jsx
@@ -1,12 +1,30 @@
 import React from "react";
-import BarraDeNavegacion from "../componentes/BarraDeNavegacion"
 import Carrusel from "../componentes/Carrusel"
 import GameLibr from "../componentes/GameLibr"
+import "../estilos/Biblioteca.css"
 
 const Biblioteca = () => {
+    const productos = [
+        { id: 1, nombre: "Call of Duty Black Ops III", precio: "S/. 217.00", categoria: ["Acción", "Multijugador"], calificacion: 4, imagen: "Call of Duty Black Ops III.png" },
+        { id: 2, nombre: "Among Us", precio: "S/.11.59", categoria: ["Acción", "Multijugador"], calificacion: 3, imagen: "Among Us.png" },
+        { id: 3, nombre: "DRAGON BALL Sparking ZERO", precio: "S/.207.50", categoria: ["Acción", "Aventura"], calificacion: 5, imagen: "DRAGON BALL Sparking ZERO.png" },
+        { id: 4, nombre: "Goat Simulator", precio: "S/.23.00", categoria: ["Acción", "Aventura"], calificacion: 4, imagen: "Goat Simulator.png" },
+        { id: 5, nombre: "HADES", precio: "S/.53.00", categoria: ["Acción", "RPG"], calificacion: 5, imagen: "HADES.png" },
+        { id: 6, nombre: "Metro Exodus", precio: "S/.75.00", categoria: ["Acción", "Aventura", "Terror"], calificacion: 4, imagen: "Metro Exodus.png" },
+        { id: 7, nombre: "Cyberpunk 2077", precio: "S/.199.00", categoria: ["Acción", "RPG"], calificacion: 4, imagen: "Cyberpunk 2077.png" },
+        { id: 8, nombre: "ELDEN RING", precio: "S/.172.50", categoria: ["Acción", "Aventura", "RPG"], calificacion: 5, imagen: "ELDEN RING.png" },
+        { id: 9, nombre: "God of War", precio: "S/.199.99", categoria: ["Acción", "Aventura"], calificacion: 5, imagen: "GOW.png" }
+    ];
+
     return <>
-    <Carrusel/>
-    <GameLibr/>
+    <div className="main-container">
+        <div className="libray-section">
+            <Carrusel/>
+        </div>
+        <div className="libray-section">
+            <GameLibr/>
+        </div>
+    </div>
         </>
 }
 
